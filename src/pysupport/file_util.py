e7a37e1d33dc541de7f9bd65b70e49b3ad6794f4
cjs <42282367@qq.com>
2025-07-22 09:51:32 +0800

    edit

--- src/pysupport/file_util.py
+++ src/pysupport/file_util.py
@@ -5,14 +5,17 @@ def file_get_contents(f: str)->str:
     if os.path.isfile(f)==False:
       return ""
 
-    with open(f, "r") as file:
+    with open(f, "r", encoding="utf-8") as file:
       data = file.read()
       return data
 
 def file_put_contents(f: str, data: str)->bool:
     """写文件内容"""
-    os.makedirs(os.path.dirname(f), exist_ok=True)
-    with open(f, "a+") as file:
+    try:
+      os.makedirs(os.path.dirname(f), exist_ok=True)
+    except Exception as e:
+      pass
+    with open(f, "a+", encoding="utf-8") as file:
       i = file.write(data + os.linesep)
       return i>0
 
